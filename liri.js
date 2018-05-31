05c242aa2c4f6ff472aa4d4f5ea5670a24c4cc98
shaheen1991 <shaheen_11991@hotmail.com>
2018-05-31 05:18:57 -0500

    commit

--- liri.js
+++ liri.js
@@ -4,7 +4,6 @@
 // * `movie-this`
 // * `do-what-it-says`
 
-//not ready for this yet
 require("dotenv").config();
 
 //movie specific variables
@@ -29,7 +28,6 @@ var input = process.argv;
 
 //grabs user command for tweets, movies, spotify or random
 var command = input[2];
-// console.log(input[2]);
 
 //grabs movie or song names to put into request
 var name = "";
@@ -38,21 +36,18 @@ for (i = 3; i < input.length; i++) {
 }
 
 name = name.trim().replace(" ", "+");
-// console.log(name);
-//console.log(nodeArgs.length);
 if (command === "my-tweets") {
 	    		
 	var params = {screen_name: 'shaheen65494263', limit: 20};
 	
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	  if (!error) {
-	  	// console.log(response, null, 2);
 	    for (var i = tweets.length - 1; i >= 0; i--) {
 	    	var myTweets = 
-	    		"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + "\r\n" + "\r\n" +
+	    		"-----------------------------------------------------------------------" + "\r\n" + "\r\n" +
 	    		"Tweet Number: " + (tweets.length-i) + "\r\n" +
 	    		"Posted on: " + tweets[i].created_at + "\r\n" +
-	    		"Tweet Posted: " + tweets[i].text + "\r\n" + "\r\n" + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	    		"Tweet Posted: " + tweets[i].text + "\r\n" + "\r\n" + "-----------------------------------------------------------------------"
 	    	console.log(myTweets);
 	    	writeToLog(myTweets);
 			} 
@@ -70,8 +65,6 @@ else if (command === "spotify-this-song") {
     	return console.log('Error occurred: ' + err);
   	}
   	
-  	// console.log(JSON.stringify(data, null, 2));
-  	// console.log(data);
   	var track = data.tracks.items[5];
     var mySong =
 		"-----------------------------------------------------------------------" + "\r\n" +
@@ -95,25 +88,19 @@ else if (command === "movie-this") {
 	// Then run a request to the OMDB API with the movie specified
 	var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
 
-	// This line is just to help us debug against the actual URL.
-	// console.log(queryUrl);
-
 	request.get(queryUrl, function(error, response, body) {
 		
-    	// console.log(response);
 	  	if (!error && response.statusCode === 200) {
-	  		//var body = JSON.parse(body);
+
 	  		for (i = 0; i < JSON.parse(body).Ratings.length; i++) {
 	  			if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
 	  				tomatoesRating = JSON.parse(body).Ratings[i].Value;
-	  				// console.log(tomatoesRating);
 	  			}
 	  			if (JSON.parse(body).Ratings[i].Source === "Internet Movie Database") {
 	  				internetRating = JSON.parse(body).Ratings[i].Value;
-	  				// console.log(internetRating);
 	  			}
 	  		}
-	  		// Display Title of the movie, Year the movie came out, IMDB Rating of the movie, Rotten Tomatoes Rating of the movie, Country where the movie was produced, Language of the movie, Plot of the movie, Actors in the movie.
+	
 	  		var myMovie =
   			"-----------------------------------------------------------------------" + "\r\n" +
     		"Movie Title: " + JSON.parse(body).Title + "\r\n" +
@@ -135,31 +122,23 @@ else if (command === "movie-this") {
 }
 
 if (command === "do-what-it-says") {
-	// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-	// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-	// Feel free to change the text in that document to test out the feature for other commands.
 	fs.readFile("random.txt", "utf8", function(error, data) {
 
   // If the code experiences any errors it will log the error to the console.
 		if (error) {
 			return console.log(error);
 		}
-		// console.log(data);
 		// Then split it by commas (to make it more readable)
 		var nameArr = data.split(",");
 		// // We will then re-display the content as an array for later use.
-		// console.log(nameArr);
 
 		name = nameArr[1]
-		// console.log(name);
 
 		spotify.search({ type: 'track', query: name, limit: 1 }, function(err, data) {
 			if (err) {
 				return console.log('Error occurred: ' + err);
 			}
 			
-			// console.log(JSON.stringify(data, null, 2));
-			// console.log(data);
 			var track = data.tracks.items[0];
 			var randomSong =
 				"-----------------------------------------------------------------------" + "\r\n" +
